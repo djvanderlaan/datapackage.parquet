8aed509b41b10222bec3ab71ba5a5f9a89c6382b
djvanderlaan <jan@ntteloos.nl>
2024-09-05 10:23:33 +0200

    Tests for parquet_reader

--- R/parquet_reader.R
+++ R/parquet_reader.R
@@ -1,5 +1,5 @@
 
-#' Read the paquet data for a Data Resource
+#' Read the parquet data for a Data Resource
 #' 
 #' @param path path to the data set. 
 #' 
@@ -11,7 +11,13 @@
 #' @param ... additional arguments are passed on to \code{\link[arrow]{read_parquet}}.
 #'
 #' @seealso
-#' Generally used by calling \code{\link{dpgetdata}}.
+#' Generally used by calling \code{\link{dpgetdata}} from the
+#' \code{datapackage} package.
+#'
+#' When the \code{datapackage.parquet} package is loaded the reader for parquet
+#' files is registered with the \code{datapackage} package. Therefore, when
+#' using \code{dpgetdata} to ge the data for a data resource for which the data
+#' is stored in a parquet file the correct reader is automatically used.
 #'
 #' @return
 #' Returns a \code{data.frame} with the data or when \code{as_connection = TRUE}
@@ -20,44 +26,23 @@
 #' @import datapackage
 #' @export
 parquet_reader <- function(path, resource, to_factor = FALSE, as_connection = FALSE, ...) {
-  schema <- dpschema(resource)
+  schema <- datapackage::dpschema(resource)
   if (is.null(schema)) {
     dta <- arrow::read_parquet(path, as_data_frame = !as_connection, ...)
   } else {
     dta <- arrow::read_parquet(path, as_data_frame = !as_connection, ...)
     if (as_connection) {
       # Check if parquet file is valid
-      # First try to convert the first few rows; this should already catch quite a few
-      # possiblee issues; e.g. levels of factor not matching code list
       tmp <- dta$Take(1) |> as.data.frame()
-      tmp <- dpapplyschema(tmp, resource, to_factor = to_factor)
-      # However, we also want an integer column to be numeric etc. dpapplyschema will
-      # accept character for most fields.
-      for (fieldname in dpfieldnames(schema)) {
-        field <- dpfield(schema, fieldname)
-        type  <- dpproperty(field, "type")
-        class <- class(tmp[[fieldname]])
-        if (is.factor(tmp[[fieldname]]) && !is.null(dpproperty(field, "codelist"))) {
-          # this is ok
-        } else if (type == "boolean") {
-          if (!methods::is(tmp[[fieldname]], "logical"))
-            stop("Field '", fieldname, "' is of wrong type. Should be a logical")
-        } else if (type == "date") {
-          if (!methods::is(tmp[[fieldname]], "Date"))
-            stop("Field '", fieldname, "' is of wrong type. Should be a Date.")
-        } else if (type == "integer") {
-          if (!methods::is(tmp[[fieldname]], "integer"))
-            stop("Field '", fieldname, "' is of wrong type. Should be an integer.")
-        } else if (type == "number") {
-          if (!methods::is(tmp[[fieldname]], "numeric"))
-            stop("Field '", fieldname, "' is of wrong type. Should be an numeric")
-        } else if (type == "string") {
-          if (!methods::is(tmp[[fieldname]], "character"))
-            stop("Field '", fieldname, "' is of wrong type. Should be an character")
-        }
-      }
+      # Then we check these records; note that this will not catch all errors as, for
+      # example fields not read in can be invalid; this is mainly the case when 
+      # checking contraints, therefore we will not check these as this would give a 
+      # false sense of safety
+      check <- datapackage::dpcheckdataresource(tmp, resource, constraints = FALSE)
+      if (!isTRUE(check)) stop(check)
     } else {
-      dta <- dpapplyschema(dta, resource, to_factor = to_factor)
+      dta <- datapackage::dpapplyschema(dta, resource, to_factor = to_factor)
+      class(dta) <- "data.frame"
     }
   }
   structure(dta, resource = resource)
